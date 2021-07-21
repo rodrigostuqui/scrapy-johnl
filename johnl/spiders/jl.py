554dabc2b3c37e5efce4afbdd6868efe5fc097a7
Rodrigo Stuqui <rst.monzani@gmail.com>
2021-07-21 09:53:01 -0300

    tamo indo

--- johnl/spiders/jl.py
+++ johnl/spiders/jl.py
@@ -2,6 +2,7 @@ from urllib import parse
 import scrapy
 from scrapy.http.request import Request
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import time
 
 class JlSpider(scrapy.Spider):
@@ -9,37 +10,34 @@ class JlSpider(scrapy.Spider):
     start_urls = ['https://www.johnlewis.com/brands']
 
     def __init__(self):
-        self.driver = webdriver.Chrome()
-
-    def parse(self, response, **kwargs):
-        category_value = response.xpath('//select[@class="brands__sort brands__sort--desktop pretty-select"]//option//@value').getall()
-
-        for count, i in enumerate(category_value):
-            if i!= '':
-                yield scrapy.Request(url = "https://www.johnlewis.com/brands?deptId={}".format(i), callback=self.parse_category)
+        self.driver = webdriver.Firefox()
 
 
-    def parse_category(self, response ,**kwargs):
-        url = "https://www.johnlewis.com"
+    def parse(self, response ,**kwargs):
         links = response.xpath('//li/a[re:test(@href, "/brand")]/@href').getall()
         for link in links:
             yield scrapy.Request(response.urljoin(link), callback = self.parse_product)
 
 
     def parse_product(self, response, **kwargs):
-        category = response.xpath('//meta[@property="og:title"]//@content').get()
         self.driver.get(response.url)
         time.sleep(3)
         self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        
-        products = self.driver.find_elements_by_class_name('GridColumn_col-sm-6__Ba6rz')
-        for product in products:
-            name = product.find_element_by_class_name('title_title__1MULs').text
-            price = product.find_element_by_class_name('price_c-product-card__price__3NI9k').text
-            yield {'category' : category,
-            'name' : name,
-            'price' : price}            
-
+        time.sleep(3)
+        products = self.driver.page_source
+        soup=BeautifulSoup(products, 'html.parser')
+        links = soup.find_all('a', class_= 'image_imageLink__1tBDW product-card_c-product-card__image__3CdTi product__image', href=True)
+        for i in links:
+            yield scrapy.Request(url="https://www.johnlewis.com{}".format(i['href']), callback=self.parse_description)
         next_page = response.xpath('//a[contains(@class, "Pagination_c-pagination__btn__2UzxY Pagination_c-pagination__next-btn__3g_DG")]/@href').get()
         if next_page:
             yield scrapy.Request(response.urljoin(next_page), callback=self.parse_product)
+
+
+    def parse_description(self, response, **kwargs):
+        name = response.xpath('//jl-store-stock//@productname').get()
+        price = response.xpath('//jl-store-stock//@skuprice').get()
+        yield{
+            'name' : name,
+        'price' : price
+        }
